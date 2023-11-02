bb8e8f40dcbdf29b3bad093c3b67d96ca2f610f3
Hans Bachman <hans.lovestanks@gmail.com>
2023-11-02 13:49:25 -0600

    Made elements into buttons

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,9 @@
+const buttonM = document.querySelector('#check');
 
-// document.getElementById('blue').addEventListener('click', changeColour);
-// document.getElementById('green').addEventListener('click', changeColour);
-
-// function changeColour() {
-//     var colour = this.getAttribute("id");
-//     console.log(colour);
-
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         chrome.tabs.sendMessage(tabs[0].id, {message: colour}, 
-//         function(response) {
-//             var newElement = document.createElement('span');
-//             newElement.textContent = response.message;
-//             document.body.appendChild(newElement);
-            
-//         });
-//     });
-// };
+buttonM.addEventListener('change', () => {
+    if (buttonM.checked) {
+        console.log('The checkbox is checked.');
+    } else {
+      console.log('The checkbox is not checked.');
+    }
+  });
--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,41 @@
 //         sendResponse({message: "Message received!"});
 //     }
 // );
+
+
+var tableData = [];
+
+const cells = document.querySelectorAll('td');
+var index = 0;
+var tableLength = 0;
+// Loop through each table cell and replace it with a button element
+cells.forEach((cell) => {
+    if (tableLength % 2 != 0) {
+        if (cell.classList.contains('mpdefault')) {
+            // Store the HTML content of the table cell in the tableData array
+            tableData[index] = cell.innerHTML;
+        
+            // Create a new button element and replace the table cell with it
+            const button = document.createElement('button');
+            button.textContent = cell.innerText;
+            cell.parentNode.replaceChild(button, cell);
+        
+            // Store the HTML content of the button element in the tableData array
+            tableData[index] = button.outerHTML;
+       
+            // Increment the index variable
+
+        };
+    };
+    index++;
+    tableLength++;
+    
+  });
+
+
+console.log(tableData);
+
+
+
+
+
