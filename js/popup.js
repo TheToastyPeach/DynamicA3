e871dfbb885d4473b3ac9b60e29270459be38cd5
Hans Bachman <hans.lovestanks@gmail.com>
2023-11-05 23:38:01 -0700

    Got the buttons working!

--- js/popup.js
+++ js/popup.js
@@ -4,14 +4,14 @@ buttonM.addEventListener('change', () => {
     if (buttonM.checked) {
         console.log('The checkbox is checked.');
 
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {message: "t"}, 
-          function(response) {
-              var newElement = document.createElement('span');
-              newElement.textContent = response.message;
-              document.body.appendChild(newElement);
-          });
-        });
+        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        //   chrome.tabs.sendMessage(tabs[0].id, {message: "t"}, 
+        //   function(response) {
+        //       var newElement = document.createElement('span');
+        //       newElement.textContent = response.message;
+        //       document.body.appendChild(newElement);
+        //   });
+        // });
     } else {
       console.log('The checkbox is not checked.');
     }
--- js/scripts.js
+++ js/scripts.js
@@ -35,27 +35,69 @@
 const tables = document.querySelectorAll('table.menuplaintable');
 //console.log(tables);
 var evenTable = 0;
-
+var href;
 
 // Loop through each table
 tables.forEach((table) => {
-    // Create a new div element
+    // Create a new div element to containt the new elements 
     const div = document.createElement('div');
+    div.classList.add('PrettyDiv');
 
     // Get all td elements in the table
     const tds = table.querySelectorAll('td');
 
     // Loop through each td
     tds.forEach((td) => {
+        //Removing the empty table elements 
+        if (evenTable % 2 != 0) {
             // Create a new div for each td element in the table 
             const tdDiv = document.createElement('div');
             tdDiv.innerHTML = td.innerHTML;
+    
+            // Replace the td element with the new div element
             td.parentNode.replaceChild(tdDiv, td);
+    
+            const aElements = tdDiv.querySelectorAll('a');
+    
+            aElements.forEach((a) => {
+                href = a.href;
+                console.log(href);
+                const tempButton = document.createElement('button');
+                tempButton.textContent = a.textContent;
+                tempButton.classList.add('btn');
+                tempButton.dataset.href = a.href;
+                
+                a.parentNode.replaceChild(tempButton, a);
+            });
+        } else {
+            // Remove the empty td element from the table
+            td.remove();
+        }
+        evenTable++;
     });
 
+    // Attach the event listener to the document and use event delegation
+    document.addEventListener('click', function (event) {
+        // If the clicked element doesn't have the right selector, bail
+        if (!event.target.matches('.btn')) return;
+        var href = event.target.dataset.href;
+        console.log("clicked");
+        window.open(href, '_blank');
+    });
+    
     div.innerHTML = table.innerHTML;
     table.parentNode.replaceChild(div, table);
 });
+
+
+// // Attach the event listener to the document and use event delegation
+// document.addEventListener('click', function (event) {
+//     // If the clicked element doesn't have the right selector, bail
+//     if (!event.target.matches('.btn')) return;
+
+//     console.log("clicked");
+//     window.open(href, '_blank');
+// });
 
 
 
